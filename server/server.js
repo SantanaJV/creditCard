83cca6d6301050ce54adaefa14730bd998ed598d
Santana <joaosantana2001@icloud.com>
2019-08-18 20:54:19 -0300

    server.js: set up connection with mongoDB atlas

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const app = require("express")();
 const bodyparser = require("body-parser");
 const rfid = require('./routes/rfid');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  '<URI>',
+ { useNewUrlParser: true}, 
+ (err) => {
+  if(err) console.log(err);
+
+  console.log("Connected to MongoDB database");
+});
 
 app.use(bodyparser.json());
 app.use("/rfid", rfid);
