0e31113b40e1258225a49ea35678605721540e67
Santana <joaosantana2001@icloud.com>
2019-08-18 20:33:55 -0300

    routes/rfid.js: create endpoint to register a rfid

--- server/routes/rfid.js
+++ server/routes/rfid.js
@@ -13,7 +13,24 @@ router.get("/:uid", async (req, res) => {
         rfid.registered = true;
         res.status(200).send(rfid);
     } catch(err) {
-        res.status(500).send("Internal error - Check server log");
+        res.status(500).send("Internal error - check server log");
+        console.log(err);
+    }
+});
+
+router.post("/", async (req, res) => {
+    try{
+        let rfid = req.body;
+        console.log(req.body);
+
+        if(!rfid.uid || !rfid.name || !rfid.grade) return res.status(400).send("Invalid request.");
+
+        rfid = new Rfid(rfid);
+        await rfid.save();
+
+        res.status(200).send(rfid);
+    } catch(err) {
+        res.status(500).send("Internal error - check server log");
         console.log(err);
     }
 });
